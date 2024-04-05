d503ef2a5daa61f3f6a3cbd397d0e2742dce8a26
homexp13 <homexp13@gmail.com>
2024-04-05 12:52:58 +0300

    tgui

--- tgui/packages/tgui/interfaces/HiveStrainScreen.tsx
+++ tgui/packages/tgui/interfaces/HiveStrainScreen.tsx
@@ -1,5 +1,5 @@
 import { useBackend } from '../backend';
-import { Button, Section, ProgressBar, Collapsible } from '../components';
+import { Button, Section, Collapsible } from '../components';
 import { Window } from '../layouts';
 
 const CasteView = (props) => {
